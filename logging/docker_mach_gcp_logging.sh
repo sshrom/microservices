36ba47223f66f3db13b442931de122409d139f12
Roman.Shekhovtsov <squidlock@gmail.com>
2020-08-09 22:46:41 +0400

    logging-1: + zipkin

--- logging/docker_mach_gcp_logging.sh
+++ logging/docker_mach_gcp_logging.sh
@@ -7,7 +7,7 @@ set -e
 
 echo "### CREATING DOCKER MACHINE:"
 docker-machine create --driver google \
- --google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1804-lts \
+--google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1804-lts \
 --google-disk-size 50 \
 --google-machine-type n1-standard-1 \
 --google-open-port 5601/tcp \
